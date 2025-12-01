7934d6d705f4d5c75197789e083540c380bb66a7
Anand6262 <pyexpert62@gmail.com>
2025-12-01 12:32:18 +0530

    chanegs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Parent from './views/Parent'
-import Child from './views/Child'
-
+import React, { useCallback, useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
+
+
+  // 1) ❌ If you DON'T use useCallback → this function is recreated on every render
+  // const handleIncrement = () => {
+  //     setCount(count + 1);
+  // };
+
+  // 1) ✅ Using useCallback (keeps same reference)
+  const handleIncrement = useCallback(() => {
+    setCount((prev) => prev + 10);
+  }, []); // no dependencies → function created only once
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Parent />} />
-        <Route path='/child' element={<Child />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <h2>Count: {count}</h2>
+      <button onClick={handleIncrement}>Increment</button>
+    </>
   )
 }
 
